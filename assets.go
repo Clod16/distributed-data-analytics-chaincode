15a3b83dbeb933e980c9154daef10dae1601fb4f
clod16 <c.barberini94@gmail.com>
2018-10-17 11:10:08 +0200

    add CreateCompositeKey() & SetEvent() in keyutils.go

---
+++ assets.go
@@ -0,0 +1,24 @@
+package main 
+
+type AnalitycsInstances struct{
+
+	Id                       string `json:"id"`
+	Payload                  string `json:"payload"`
+	Egid					 string `json:"egid"`	
+	EdgeGateway				 		`json:"edgeGateway"`
+}
+
+type  DataSource struct {
+
+	Id                       string `json:"id"`
+	Payload                  string `json:"payload"`
+	Egid					 string `json:"egid"`			
+	//EdgeGateway				 		`json:"edgeGateway"`
+}
+
+
+type EdgeGateway struct{
+
+	Egid                       string `json:"egid"`
+	Payload                  string `json:"payload"`
+}
--- dda-chaincode.go
+++ dda-chaincode.go
@@ -62,7 +62,7 @@ func (t *DistributedDataAnalyticsChaincode) Invoke(stub shim.ChaincodeStubInterf
 	}
 
 	function, args := stub.GetFunctionAndParameters()
-	
+
 	if function == "createAnalyticsInstances" {
 		return t.createAnalyticsInstances(stub, isEnabled, args)
 	} else if function == "updateAnalyticsInstances" {
@@ -88,19 +88,109 @@ func (t *DistributedDataAnalyticsChaincode) Invoke(stub shim.ChaincodeStubInterf
 	} else if function == "updateEdgeGateways" {
 		return t.updateEdgeGateways(stub, isEnabled, args)
 	} else if function == "getEdgeGateways" {
-	   return t.getEdgeGateways(stub, isEnabled)
-    } else if function == "getEdgeGatewaysByEgid" {
-	   return t.getEdgeGatewaysByEgid(stub, isEnabled, args)
-    } else if function == "deleteEdgeGatewaysByEgid" {
+		return t.getEdgeGateways(stub, isEnabled)
+	} else if function == "getEdgeGatewaysByEgid" {
+		return t.getEdgeGatewaysByEgid(stub, isEnabled, args)
+	} else if function == "deleteEdgeGatewaysByEgid" {
 		return t.deleteEdgeGatewaysByEgid(stub, isEnabled, args)
-	 }
+	}
 	return shim.Error("Invalid invoke function name")
 }
+func (t *DistributedDataAnalyticsChaincode) updateAnalyticsInstancesstub(shim.ChaincodeStubInterface, isEnabled bool, args []string) {
+
+	var analytics AnalitycsInstances
+	logger.info(" updateAnalyticsInstancesstub()\n")
+
+	if len(args) != 3{
+		return shim.Error("updateAnalyticsInstancesstub() ERROR: must exactly 3 args!!! ")
+	}
+
+	DDAKey, err := getDDAKey(stub, args[0], args[1]){
+		if err != nil{
+			return shim.Error("CreateCompositeKey() ERROR: "err.Error())
+		}
+	}
+
+	DDABytes, err := stub.GetState(DDAKey)
+	if err!= nil{
+		return shim.Error("GetState() ERROR: " err.Error())
+	}
+	
+	err := json.Unmarshal([]byte(DDABytes), &analytics)
+	if err != nil{
+		return shim.Error("json.Unmarshal() ERROR: ", err.Error())
+	}
+
+	newPayload = args[1]
+	analytics.Payload = newPayload
+	err :=stub.PutState(DDAKey, newPayload)
+	if err != nil{
+		return shim.Error("PutState() ERROR: " err.Error())
+	}
+	jsonDDA, err := json.Marshal(&analytics)
+	if err != nil{
+		return shim.Error("json.Marshal() ERROR: " err.Error())
+	}
+	err := setDDAEvent(stub, jsonDDA)
+	if err != nil{
+		return shim.Error(" setEvent() ERROR: " err.Error())
+	}
 
 
 
 
 
+}
+func (t *DistributedDataAnalyticsChaincode) createAnalyticsInstances(stub shim.ChaincodeStubInterface, isEnabled bool, args []string) {
+
+	var analytics AnalitycsInstances
+	var analyticsEvent AnalitycsInstances
+	var analytcsID, analytcsEGID string
+	var payload string
+	logger.info(" createAnalyticsInstances()\n")
+
+	if len(args) == 2 {
+		err := json.Unmarshal([]byte(args[0], &analytics))
+		if (len(analytics.Id) == 0){
+			analytcsID = xid.New()
+			analytcsEGID = args[1]
+			payload = args[0]
+		} else {
+			analyticsID = analytics.Id
+			analytcsEGID = args[1]
+			payload = analytics.Payload
+		}
+	} else {
+		analytcsID = args[0]
+		analytcsEGID = args[2]
+		payload = args[1]
+	}
+
+	DDAKey, err := getDDAKey(stub, analytcsID, analytcsEGID)
+	if err!= nil{
+		return shim.Error("CreateCompositeKey() ERROR: "err.Error())
+	}
+
+
+	err :=stub.PutState(DDAKey, payload)
+	if err != nil{
+		return shim.Error("PutState() ERROR: " err.Error())
+	}
+
+	analyticsEvent.Id = analytcsID
+	analyticsEvent.Egid = analytcsEGID
+	analyticsEvent.Payload = payload
+	jsonDDA, err := json.Marshal(&analyticsEvent)
+	if err != nil{
+		return shim.Error("json.Marshal() ERROR: " err.Error())
+	}
+	err := setDDAEvent(stub, jsonDDA)
+	if err != nil{
+		return shim.Error(" setEvent() ERROR: " err.Error())
+	}
+
+
+}
 
 func main() {
 	twc := new(DistributedDataAnalyticsChaincode)
